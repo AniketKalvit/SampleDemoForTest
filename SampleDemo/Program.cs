339749ac4110379e822c61c4c06de37cfaa54c12
aniketkalvit <aniketkalvit@gmail.com>
2023-06-09 11:41:04 +0530

    Add project files.

---
+++ SampleDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a number");
+int num=Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.WriteLine($"{num} is even");// true
+}
+else
+{
+    Console.WriteLine($"{num} is odd"); // false
+}
